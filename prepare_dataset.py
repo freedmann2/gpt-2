8f1e569b3aeff1c8fc040fa3521843f3ea692b29
Shawn Presser <shawnpresser@gmail.com>
2020-09-08 17:49:57 -0700

    prepare_dataset.py: try multiprocessing

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -11,28 +11,39 @@ from ftfy import fix_text
 
 import tflex_utils
 
+from multiprocessing import Pool, cpu_count
+
 parser = argparse.ArgumentParser(
     description='Use FTFY to prepare a dataset for training.',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('infile', metavar='PATH', type=str, help='Input file, directory, or glob pattern (utf-8 text).')
 parser.add_argument('--outfile', default="-", type=str, help='Output file path, or - for stdout')
+parser.add_argument('--nproc', default=0, type=int, help='Number of processes to use')
+
+def fix(line):
+  fixed = fix_text(line)
+  # replace unicode … with ... which ftfy doesn't do by default
+  # NOTE: this departs from openai's convention of calling
+  # ftfy.fix_text() with default arguments. In particular,
+  # OpenAI's GPT-2 models do generate unicode ellipses.
+  # Nonetheless, we replace unicdoe ellipses with ... to
+  # increase the chances of semantic understanding.
+  fixed = fixed.replace(' …', '...') # first pass: convert "foo  …" to "foo..."
+  #fixed = fixed.replace(' …', '...') # second pass: convert "foo …" to "foo..."
+  fixed = fixed.replace('…', '...') # final pass: convert "foo…" to "foo..."
+  return fixed
 
 def main():
     args = parser.parse_args()
     out = sys.stdout if args.outfile == '-' else open(args.outfile, "w")
     i = 0
-    with open(args.infile) as f:
+    nproc = args.nproc or cpu_count()
+    pool = Pool(processes=nproc) if nproc > 1 else None
+    mapper = pool.imap if nproc > 1 else map
+    with open(args.infile, encoding='utf-8') as f:
       for i, line in tflex_utils.for_each_line(f):
-        fixed = fix_text(line)
-        # replace unicode … with ... which ftfy doesn't do by default
-        # NOTE: this departs from openai's convention of calling
-        # ftfy.fix_text() with default arguments. In particular,
-        # OpenAI's GPT-2 models do generate unicode ellipses.
-        # Nonetheless, we replace unicdoe ellipses with ... to
-        # increase the chances of semantic understanding.
-        fixed = fixed.replace(' …', '...') # first pass: convert "foo  …" to "foo..."
-        #fixed = fixed.replace(' …', '...') # second pass: convert "foo …" to "foo..."
-        fixed = fixed.replace('…', '...') # final pass: convert "foo…" to "foo..."
+        fixed = list(mapper(fix, [line]))
+        fixed = fixed[0]
         out.write(fixed)
         i += 1
         if i % 100 == 0:
